b5e883b882fc9ddd9d5d7701805d1dbe737fcd91
Timothee20V <timothee.viardtv@gmail.com>
2022-10-25 15:08:34 +0200

    jegbfej

--- UI.py
+++ UI.py
@@ -11,16 +11,16 @@ def jeu():
     fenetre.attributes('-fullscreen', True)
 
     # Zone de dessin pour le jeu
-    leJeu = Frame(fenetre, width=500, height=400, bg='green')
-    leJeu.pack()
+    leJeu = Frame(fenetre, width=1000, height=400, bg='green')
+    leJeu.pack(side='left')
 
     # Zone de dessin pour la grille
     laGrille = Frame(leJeu, width=200, height=200, bg='red')
-    laGrille.pack()
+    laGrille.pack(side='right')
 
     # Zone de dessin pour les informations
-    informations = Frame(fenetre, width=500, height=400, bg='blue')
-    informations.pack()
+    informations = Frame(fenetre, width=536, height=400, bg='blue')
+    informations.pack(side='right')
 
     fenetre.bind('<Escape>', lambda e: fenetre.destroy())
 
