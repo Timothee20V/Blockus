9fa3ad17e5aff3d5bc0d0dd3a8503becd49876e4
Timothee20V <timothee.viardtv@gmail.com>
2022-10-25 15:42:38 +0200

    Description du fichier main

--- UI.py
+++ UI.py
@@ -11,16 +11,12 @@ def jeu():
     fenetre.attributes('-fullscreen', True)
 
     # Zone de dessin pour le jeu
-    leJeu = Frame(fenetre, width=1000, height=400, bg='green')
-    leJeu.pack(side='left')
-
-    # Zone de dessin pour la grille
-    laGrille = Frame(leJeu, width=200, height=200, bg='red')
-    laGrille.pack(side='right')
+    leJeu = Canvas(fenetre, width=1000, height=400, bg='green')
+    leJeu.grid(row=0, column=0)
 
     # Zone de dessin pour les informations
-    informations = Frame(fenetre, width=536, height=400, bg='blue')
-    informations.pack(side='right')
+    informations = Canvas(fenetre, width=525, height=400, bg='blue')
+    informations.grid(row=0, column=1)
 
     fenetre.bind('<Escape>', lambda e: fenetre.destroy())
 
