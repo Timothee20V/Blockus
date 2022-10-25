d4308b9618fe504ea932214c0a455b5999c3c55a
Mathis DIBKE <mathis.dibke@reseau.eseo.fr>
2022-10-25 14:55:36 +0200

    ajout fonction liste mémoire
    ajoute la fonction mémoire des pièces utilisés

--- piece.py
+++ piece.py
@@ -2,6 +2,10 @@ class Joueur:
 
     def __init__(self,couleur):
         self.couleur = couleur
+        self.piecesMain = [i for i in range(1,22)]
+
+    def restePieces(self,piece_Utilisée):
+        self.piecesMain[piece_Utilisée-1] = 0
 
     def creationPieces (self,piece_Numero):
         nom_Piece_Liste = ["pieces/piece1","pieces/piece2","pieces/piece3","pieces/piece4","pieces/piece5","pieces/piece6","pieces/piece7",
@@ -11,6 +15,7 @@ class Joueur:
         with open(nom_Piece_Liste[piece_Numero-1], "r") as f:
             for ligne in f:
                 piece_Select.append(ligne.strip().split(' '))
+            self.restePieces(piece_Numero) 
         return piece_Select
 
 tableau = Joueur("bleu")
