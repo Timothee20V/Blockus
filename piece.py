123120a0bb3ee0b2fd3b92723944f4f5e2a74b9a
Mathis DIBKE <mathis.dibke@reseau.eseo.fr>
2022-10-27 08:43:45 +0200

    Correctif

--- piece.py
+++ piece.py
@@ -15,8 +15,25 @@ class Joueur:
         del self.nom_Piece_Liste[piece_Utilisée]
         print(self.nom_Piece_Liste)
 
+    def couleurEquipe(self,piece_Selectionnee):
+        if(self == "Bleu"):
+            for longueur in range(5):
+                for largeur in range(5):
+                    if(piece_Selectionnee[longueur][largeur] == 1):
+                        piece_Selectionnee[longueur][largeur] = "B"
+        elif(self == "Rouge"):
+            for longueur in range(5):
+                for largeur in range(5):
+                    if(piece_Selectionnee[longueur][largeur] == 1):
+                        piece_Selectionnee[longueur][largeur] = "R"
+        elif(self == "Jaune"):
+            for longueur in range(5):
+                for largeur in range(5):
+                    if(piece_Selectionnee[longueur][largeur] == 1):
+                        piece_Selectionnee[longueur][largeur] = "R" 
+
     #récupération du modèle de la pièce et suppression de celle-ci dans le tableau 
-    def creationPieces (self,piece_Numero):
+    def creationPieces(self,piece_Numero):
         #référencement des pièces disponibles
         piece_Selectionnee_Model = []
         #lecture du fichier choisit et récupération du modèle
@@ -25,4 +42,20 @@ class Joueur:
                 piece_Selectionnee_Model.append(ligne.strip().split(' '))
             #suppression de la pièce dans la liste des pièces restantes
             self.restePieces(piece_Numero)
+            piece_Selectionnee_Model = self.
         return piece_Selectionnee_Model
+
+    def rotation_Pieces(self,piece_Selectionnee,rotation):
+        piece_Retournee = piece_Selectionnee
+        if(rotation == "Gauche"):
+            for longueur in range(5):
+                for largeur in range(5):
+                    piece_Retournee[longueur][largeur] = piece_Selectionnee[5-largeur-1][longueur]
+        elif(rotation == "Droite"):
+            for longueur in range(len(piece_Retournee)):
+                for largeur in range(len(piece_Retournee[longueur])):
+                    piece_Retournee[longueur][largeur] = piece_Selectionnee[5-largeur-1][longueur]
+        return piece_Retournee
+
+Bleu = Joueur("Bleu")
+print(Bleu.creationPieces(5))
