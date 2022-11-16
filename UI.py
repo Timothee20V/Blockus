242c8805bef7eda030adb644383de2f023470fb3
Timothee <timothee.viardtv@gmail.com>
2022-11-16 20:15:46 +0100

    Renomination of constants

--- UI.py
+++ UI.py
@@ -1,44 +1,44 @@
 from tkinter import *
 
 
-class Grille:
-    def __init__(self, x, y, size):
+class Grid:
+    def __init__(self, x, y, sizeCase):
         self.offsetX = x
         self.offsetY = y
-        self.tailleCase = size
+        self.sizeCase = sizeCase
 
-        fenetre = Tk()
-        fenetre.title("Blokus")
-        fenetre.attributes('-fullscreen', True)
+        window = Tk()
+        window.title("Blokus")
+        window.attributes('-fullscreen', True)
 
-        game = Canvas(fenetre, width=1000, height=860)
+        game = Canvas(window, width=1000, height=860)
 
         self.creationGrid(game)
 
         game.grid(row=0, column=0)
         game.create_rectangle(100, 30, 900, 830)
 
-        informations = Canvas(fenetre, width=528, height=860)
+        informations = Canvas(window, width=528, height=860)
         informations.grid(row=0, column=1)
 
-        fenetre.bind('<Escape>', lambda e: fenetre.destroy())
+        window.bind('<Escape>', lambda e: window.destroy())
 
-        fenetre.mainloop()
+        window.mainloop()
 
     def creationGrid(self, game):
         for i in range(1, 20):
-            game.create_line(i * self.tailleCase + self.offsetX,
+            game.create_line(i * self.sizeCase + self.offsetX,
                              self.offsetY,
-                             i * self.tailleCase + self.offsetX,
+                             i * self.sizeCase + self.offsetX,
                              800 + self.offsetY
                              )
 
         for i in range(1, 20):
             game.create_line(self.offsetX,
-                             self.offsetY + i * self.tailleCase,
+                             self.offsetY + i * self.sizeCase,
                              self.offsetX + 800,
-                             self.offsetY + i * self.tailleCase
+                             self.offsetY + i * self.sizeCase
                              )
 
 
-jeu = Grille(100, 30, 40)
+jeu = Grid(100, 30, 40)
