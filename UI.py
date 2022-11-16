7cd197ecee78c36c6c1e9e836c1a7345fa365870
Timothee <timothee.viardtv@gmail.com>
2022-11-16 20:03:22 +0100

    Bugfix class Grille

--- UI.py
+++ UI.py
@@ -7,43 +7,38 @@ class Grille:
         self.offsetY = y
         self.tailleCase = size
 
-    def creationGrilleAffiche(self, other):
+        fenetre = Tk()
+        fenetre.title("Blokus")
+        fenetre.attributes('-fullscreen', True)
+
+        game = Canvas(fenetre, width=1000, height=860)
+
+        self.creationGrid(game)
+
+        game.grid(row=0, column=0)
+        game.create_rectangle(100, 30, 900, 830)
+
+        informations = Canvas(fenetre, width=528, height=860)
+        informations.grid(row=0, column=1)
+
+        fenetre.bind('<Escape>', lambda e: fenetre.destroy())
+
+        fenetre.mainloop()
+
+    def creationGrid(self, game):
         for i in range(1, 20):
-            leJeu.create_line(i * self.tailleCase + self.offsetX,
-                              self.offsetY,
-                              i * self.tailleCase + self.offsetX,
-                              800 + self.offsetY
-                              )
+            game.create_line(i * self.tailleCase + self.offsetX,
+                             self.offsetY,
+                             i * self.tailleCase + self.offsetX,
+                             800 + self.offsetY
+                             )
 
         for i in range(1, 20):
-            leJeu.create_line(self.offsetX,
-                              self.offsetY + i * self.tailleCase,
-                              self.offsetX + 800,
-                              self.offsetY + i * self.tailleCase
-                              )
+            game.create_line(self.offsetX,
+                             self.offsetY + i * self.tailleCase,
+                             self.offsetX + 800,
+                             self.offsetY + i * self.tailleCase
+                             )
 
 
 jeu = Grille(100, 30, 40)
-
-fenetre = Tk()
-# Le nom du jeu
-fenetre.title("Blokus")
-# La taille de la fenetre
-fenetre.attributes('-fullscreen', True)
-
-# Zone de dessin pour le jeu
-leJeu = Canvas(fenetre, width=1000, height=860)
-
-jeu.creationGrilleAffiche(leJeu)
-
-leJeu.grid(row=0, column=0)
-
-leJeu.create_rectangle(100, 30, 900, 830)
-
-# Zone de dessin pour les informations
-informations = Canvas(fenetre, width=528, height=860)
-informations.grid(row=0, column=1)
-
-fenetre.bind('<Escape>', lambda e: fenetre.destroy())
-
-fenetre.mainloop()
