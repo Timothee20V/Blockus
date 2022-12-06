92b2a02382507408cf93f363ec09e108f2e78e04
Mathis DIBKE <mathis.dibke@reseau.eseo.fr>
2022-12-06 20:37:49 +0100

    Load piece

--- save_load.py
+++ save_load.py
@@ -1,14 +1,28 @@
 def saveGame(plateau):
     with open("save", "w") as file:
+        file.write("Plateau")
         for ligne in range(len(plateau)):
             for colonne in plateau[ligne]:
                 file.writelines(colonne)
                 file.write(" ")
             file.write("\n")
+        file.write("Piece")
+
              
-def loadGame():
+def loadGameBoard():
     returnplateau = []
     with open("save", "r") as file:
-        for line in file:
-            returnplateau.append(line.strip().split(' '))
+        for i in file:
+            if(i=="PLateau"):
+                for line in i:
+                    returnplateau.append(line.strip().split(' '))
     return returnplateau
+
+def loadGamePiece():
+    returnpiece = {}
+    with open("save", "r") as file:
+        for i in file:
+            if(i=="Piece"):
+                for line in i:
+                    returnpiece.append(line.strip().split(' '))
+    return returnpiece
