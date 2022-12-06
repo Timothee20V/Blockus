f81d86765e1a3c557f8cfa6414f81896a8f76d8c
Mathis DIBKE <mathis.dibke@reseau.eseo.fr>
2022-12-06 20:53:34 +0100

    Load and save propre

--- save_load.py
+++ save_load.py
@@ -1,11 +1,13 @@
 def saveGame(plateau):
     with open("save", "w") as file:
         file.write("Plateau")
+        file.write("\n")
         for ligne in range(len(plateau)):
             for colonne in plateau[ligne]:
                 file.writelines(colonne)
                 file.write(" ")
             file.write("\n")
+        file.write("\n")
         file.write("Piece")
 
              
@@ -22,7 +24,11 @@ def loadGamePiece():
     returnpiece = {}
     with open("save", "r") as file:
         for i in file:
-            if(i=="Piece"):
+            while(i=="Piece"):
                 for line in i:
                     returnpiece.append(line.strip().split(' '))
     return returnpiece
+
+ref_couleur = ['rouge','jaune','vert','bleu','orange','blanc','violet','fuchsia']
+saveGame(ref_couleur)
+print(loadGameBoard())
